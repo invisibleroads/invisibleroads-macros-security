88c79cdeb5f68b2e6f1ad0df10c62926da2d802c
Roy Hyunjin Han <rhh@crosscompute.com>
2023-02-10 07:50:12 -0500

    Add get_hash

--- invisibleroads_macros_security/__init__.py
+++ invisibleroads_macros_security/__init__.py
@@ -1,9 +1,16 @@
 import secrets
-from string import digits, ascii_letters
+from hashlib import blake2b
+from string import ascii_letters, digits
 
 
-ALPHABET = digits + ascii_letters
+ALPHABET = ascii_letters + digits
 
 
 def make_random_string(length, alphabet=ALPHABET):
     return ''.join(secrets.choice(alphabet) for _ in range(length))
+
+
+def get_hash(text):
+    h = blake2b()
+    h.update(text.encode())
+    return h.hexdigest()
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
 
 setup(
     name='invisibleroads-macros-security',
-    version='1.0.2',
+    version='1.0.3',
     description='Shortcut functions for security operations',
     long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
